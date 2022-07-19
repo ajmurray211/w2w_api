73b7a2d6d53771782a40d351267b5e5f8218b981
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-07-19 16:55:49 -0400

    deployment prep

--- w2w_api/settings.py
+++ w2w_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-d-dp1p53t*j0=r&a%=!dykveq)c!#%gjrljsitr4-x&99urwm%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://quiet-wave-27100.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -116,10 +116,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+# import django_heroku
+# django_heroku.settings(locals())
