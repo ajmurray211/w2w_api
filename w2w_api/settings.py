2fb016ac3a5c3045cdefc3a3b6c9eb767aad66fc
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-07-19 18:30:01 -0400

    running into error code 500 in heroku log

--- w2w_api/settings.py
+++ w2w_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d-dp1p53t*j0=r&a%=!dykveq)c!#%gjrljsitr4-x&99urwm%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [  
     '0.0.0.0',
